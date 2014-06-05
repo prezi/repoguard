86fea278ba600b623cb2f52b859af290f55bd78c
Attila Zseller <attila.zseller@prezi.com>
2014-06-05 10:56:57 +0200

    command line tool to manually check a commit

--- repoguard/repominer.py
+++ repoguard/repominer.py
@@ -1,9 +1,12 @@
 import argparse
 import re
+import os
+import os.path
 
 from codechecker import CodeCheckerFactory
 from evaluators import LineEvalFactory
 from ruleparser import load_rules, build_resolved_ruleset
+
 
 parser = argparse.ArgumentParser(description='Check a sourcecode repo')
 parser.add_argument('--rules','-r', default="etc/", help='Directory of rules')
@@ -22,7 +25,21 @@ applied_alerts = {aid: adata for aid, adata
 		
 code_checker = CodeCheckerFactory(applied_alerts).create(LineEvalFactory.MODE_SINGLE)
 
-for fname in args.files:
-	with open(fname) as f:
-		content = f.readlines()
-		print code_checker.check(content, fname)
+alerts = []
+for path in args.files:
+	if os.path.isdir(path):
+		for root, subFolders, files in os.walk(path):
+			for fname in files:
+				with open(root + "/" + fname) as f:
+					content = f.readlines()
+					actual_alert = [(fname, alert, line) for alert, line in code_checker.check(content, fname)]
+					alerts.extend(actual_alert)
+	else:
+		with open(path) as f:
+			content = f.readlines()
+			alerts.extend(code_checker.check(content, path))
+
+for fname, alert, line in alerts:
+	print "%s\n%s\n%s\n\n" % (fname, alert, line.strip())
+
+
