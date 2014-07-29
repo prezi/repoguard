a341a0ce52ddeea2a66adb90c87f402f09f2d5d7
robert.kiss <robert.kiss@prezi.com>
2014-07-29 12:28:05 +0200

    remove prezi deployment stuff

--- fabfile.py
+++
@@ -1,8 +0,0 @@
-# fabfile.py: Used in MissionControl for placement deploys
-from prezi.fabric.placement import CommonTasks, PlacementDeploy
-
-tasks = CommonTasks(PlacementDeploy(egg_name='repoguard'), 'repoguard', {}, '/')
-
-
-def repoguard(*args, **kwargs):
-    tasks.deploy(*args, **kwargs)
--- scripts/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-# Example script that sets up the service's environment. #dev#build#tests#idoc
-
-set -e
-
-. $(dirname $0)/common
-
-ensure_virtualenv
